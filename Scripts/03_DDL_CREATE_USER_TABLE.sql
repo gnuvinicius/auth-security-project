fa2c15d4f330593cf8b625575ca732e6e673cb88
Vinicius Claudino <gnu.vinicius@gmail.com>
2022-09-17 23:20:23 -0300

    scripts to create database and tables

---
+++ Scripts/03_DDL_CREATE_USER_TABLE.sql
@@ -0,0 +1,21 @@
+-- DROP TABLE IF EXISTS public.tb_user;
+
+CREATE TABLE IF NOT EXISTS public.tb_user
+(
+    user_id integer NOT NULL DEFAULT nextval('tb_user_user_id_seq'::regclass),
+    username character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    role character varying(10) COLLATE pg_catalog."default",
+    CONSTRAINT tb_user_pkey PRIMARY KEY (user_id),
+    CONSTRAINT tb_user_username_key UNIQUE (username)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.tb_user
+    OWNER to postgres;
+
+GRANT ALL ON TABLE public.tb_user TO garage474_dev;
+
+GRANT ALL ON TABLE public.tb_user TO postgres;
